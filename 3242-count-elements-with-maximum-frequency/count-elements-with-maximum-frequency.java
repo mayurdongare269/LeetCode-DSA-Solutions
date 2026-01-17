3fa581aba8e52fe249e350b72eccfd3c7ab86ac9
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-01-18 01:01:26 +0530

    Time: 2 ms (70.54%) | Memory: 44 MB (72.34%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,22 +1,79 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        int[] freq = new int[101];  // since 1 <= nums[i] <= 100
-        int maxFreq = 0;
-        
-        // Count frequency of each number
-        for (int num : nums) {
-            freq[num]++;
-            maxFreq = Math.max(maxFreq, freq[num]);
+
+        //i... 
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
-        // Count total elements that have frequency == maxFreq
-        int result = 0;
-        for (int f : freq) {
-            if (f == maxFreq) {
-                result += f;
+
+        int maxFreq = 0;
+        for(int freq : map.values()) {
+            maxFreq = Math.max(maxFreq, freq);
+        }
+
+        int cnt = 0;
+        for(int freq : map.values()) {
+            if(maxFreq == freq) {
+                cnt++;
             }
         }
+
+        return cnt * maxFreq;
+
+
+
+        //ii... for all types but not for this que
+        // i had write for both min/max freq...
+        // int n = nums.length;
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // //count frequencies
+        // for(int i = 0; i < nums.length; i++) {
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        // }
+
+        // int maxFreq = 0, minFreq = n;
+        // int maxElm = 0, minElm = 0;     
+
+        // for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        //     int elm = entry.getKey();
+        //     int cnt = entry.getValue();
+
+        //     if(cnt > maxFreq) {
+        //         maxFreq = cnt;
+        //         maxElm = elm;
+        //     }
+
+        //     if(cnt < minFreq) {
+        //         minFreq = cnt;
+        //         minElm = elm;
+        //     }
+        // }   
+
+        // return maxFreq;
+
+        //iii.. dont refer this one...
+
+        // int[] freq = new int[101];  // since 1 <= nums[i] <= 100
+        // int maxFreq = 0;
         
-        return result;
+        // // Count frequency of each number
+        // for (int num : nums) {
+        //     freq[num]++;
+        //     maxFreq = Math.max(maxFreq, freq[num]);
+        // }
+        
+        // // Count total elements that have frequency == maxFreq
+        // int result = 0;
+        // for (int f : freq) {
+        //     if (f == maxFreq) {
+        //         result += f;
+        //     }
+        // }
+        
+        // return result;
     }
 }
