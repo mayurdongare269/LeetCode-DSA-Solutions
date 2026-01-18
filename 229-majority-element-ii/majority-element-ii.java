e3044ae911edf792926c244539ac63443640b34f
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-01-18 12:00:16 +0530

    Time: 17 ms (16.01%) | Memory: 52.6 MB (42.61%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -18,40 +18,65 @@ public class Main {
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int count1 = 0, count2 = 0;
-        int candidate1 = 0, candidate2 = 0;
+        //using HashMap
 
-        // Step 1: Find candidates
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            if(map.containsKey(nums[i])) { // already exists so increase freq by one
+                map.put(nums[i], map.get(nums[i]) + 1);
             } else {
-                count1--;
-                count2--;
+                map.put(nums[i], 1); // add new key with freq 1...
             }
         }
 
-        // Step 2: Validate the candidates
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+        List<Integer> ans = new ArrayList<>();
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > n/3) {
+                ans.add(entry.getKey());
+            }
         }
 
-        List<Integer> result = new ArrayList<>();
-        int n = nums.length;
-        if (count1 > n / 3) result.add(candidate1);
-        if (count2 > n / 3) result.add(candidate2);
+        return ans;
 
-        return result;
+
+
+
+    //     int count1 = 0, count2 = 0;
+    //     int candidate1 = 0, candidate2 = 0;
+
+    //     // Step 1: Find candidates
+    //     for (int num : nums) {
+    //         if (num == candidate1) {
+    //             count1++;
+    //         } else if (num == candidate2) {
+    //             count2++;
+    //         } else if (count1 == 0) {
+    //             candidate1 = num;
+    //             count1 = 1;
+    //         } else if (count2 == 0) {
+    //             candidate2 = num;
+    //             count2 = 1;
+    //         } else {
+    //             count1--;
+    //             count2--;
+    //         }
+    //     }
+
+    //     // Step 2: Validate the candidates
+    //     count1 = 0;
+    //     count2 = 0;
+    //     for (int num : nums) {
+    //         if (num == candidate1) count1++;
+    //         else if (num == candidate2) count2++;
+    //     }
+
+    //     List<Integer> result = new ArrayList<>();
+    //     int n = nums.length;
+    //     if (count1 > n / 3) result.add(candidate1);
+    //     if (count2 > n / 3) result.add(candidate2);
+
+    //     return result;
     }
 }
