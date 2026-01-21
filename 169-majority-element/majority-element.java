c153803b54dfdc47b4599abcae9bb76d8114bf76
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-01-21 12:39:32 +0530

    Time: 13 ms (30.06%) | Memory: 52.8 MB (74.68%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,31 @@
 class Solution {
     // mayur dongare
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
+        // int count = 0;
+        // int candidate = 0;
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-            count += (num == candidate) ? 1 : -1;
+        // for (int num : nums) {
+        //     if (count == 0) {
+        //         candidate = num;
+        //     }
+        //     count += (num == candidate) ? 1 : -1;
+        // }
+
+        // return candidate;
+        int n = nums.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        return candidate;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > n/2) {
+                return entry.getKey();
+            }
+        }
+
+        return -1;
     }
 }
