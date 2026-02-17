9961fef76e25967917deb95c3000d87c185bbf90
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-02-18 00:39:48 +0530

    Time: 7 ms (32.35%) | Memory: 46.7 MB (15.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,43 +1,43 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        }
+        // for(int i = 0; i < s.length(); i++) {
+        //     map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        // }
 
-        for(int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            if(map.get(ch) != null) {
-                if(map.get(ch) == 1) { // only one freq then remove whole char
-                    map.remove(ch);
-                } else {
-                    map.put(ch, map.get(ch) - 1); // decrease freq one by one 
-                }
-            } else {
-                return false;
-            }
-        }
+        // for(int i = 0; i < t.length(); i++) {
+        //     char ch = t.charAt(i);
+        //     if(map.get(ch) != null) {
+        //         if(map.get(ch) == 1) { // only one freq then remove whole char
+        //             map.remove(ch);
+        //         } else {
+        //             map.put(ch, map.get(ch) - 1); // decrease freq one by one 
+        //         }
+        //     } else {
+        //         return false;
+        //     }
+        // }
 
-        return map.isEmpty();
+        // return map.isEmpty();
 
 
         //M2: 
 
-        // s = s.toLowerCase();
-        // t = t.toLowerCase();
+        s = s.toLowerCase();
+        t = t.toLowerCase();
 
-        // if(s.length() == t.length()) {
-        //     char[] s1 = s.toCharArray();
-        //     char[] s2 = t.toCharArray();
+        if(s.length() == t.length()) {
+            char[] s1 = s.toCharArray();
+            char[] s2 = t.toCharArray();
 
-        //     Arrays.sort(s1);
-        //     Arrays.sort(s2);
+            Arrays.sort(s1);
+            Arrays.sort(s2);
 
-        //     return Arrays.equals(s1, s2);
-        // } else {
-        //     return false;
-        // }
+            return Arrays.equals(s1, s2);
+        } else {
+            return false;
+        }
 
 
 
