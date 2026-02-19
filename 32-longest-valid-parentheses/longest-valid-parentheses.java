b23bdd262f34336e0782d5753afb67c00a2de626
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-02-19 11:00:35 +0530

    Time: 5 ms (79.98%) | Memory: 46.9 MB (6.45%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,22 +1,52 @@
 class Solution {
     public int longestValidParentheses(String s) {
+        // Stack<Integer> st = new Stack<>();
+        // st.push(-1);
+        // int maxLen = 0;
+
+        // for(int i = 0; i < s.length(); i++) {
+        //     if(s.charAt(i) == '(') {
+        //         st.push(i);
+        //     } else {
+        //         st.pop();
+        //         if(!st.isEmpty()) {
+        //             maxLen = Math.max(maxLen, i - st.peek());
+        //         } else {
+        //             st.push(i);
+        //         }
+        //     }
+        // }
+
+        // return maxLen;
+
+
+
+
+
+
+
+
+
+
         Stack<Integer> st = new Stack<>();
         st.push(-1);
-        int maxLen = 0;
+        int cnt = 0;
+        int n = s.length();
 
-        for(int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < n; i++) {
             if(s.charAt(i) == '(') {
                 st.push(i);
             } else {
                 st.pop();
-                if(!st.isEmpty()) {
-                    maxLen = Math.max(maxLen, i - st.peek());
-                } else {
+                if(st.isEmpty()) {
                     st.push(i);
+                } else {
+                    cnt = Math.max(cnt, i - st.peek());
                 }
             }
         }
 
-        return maxLen;
+        return cnt;
+
     }
 }
