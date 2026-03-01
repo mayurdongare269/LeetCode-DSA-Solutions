4c256cc112401dc43ab2e304c4ec5e9476825dfc
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-03-01 14:06:05 +0530

    Time: 2596 ms (6.87%) | Memory: 50.8 MB (86.01%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -2,23 +2,38 @@ class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         // Method1: Brute O(n2)
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        map.put(0, 1); // most imp
-
-        int ans = 0;
-        int sum = 0;
+        int cnt = 0;
         
-        for(int i = 0; i < n; i++) {
-            sum += nums[i];
+        for(int i = 0;  i<n; i++) {
+            int sum = 0;
+            for(int j = i; j < n; j++) {
+                sum += nums[j];
 
-            if(map.containsKey(sum - goal)) {
-                ans += map.get(sum - goal);
+                if(sum == goal) cnt++;
             }
-
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
+        
+        return cnt;
 
-        return ans;
+        // //method2: Better O(n) & O(n)
+        // int n = nums.length;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // map.put(0, 1); // most imp
+
+        // int ans = 0;
+        // int sum = 0;
+        
+        // for(int i = 0; i < n; i++) {
+        //     sum += nums[i];
+
+        //     if(map.containsKey(sum - goal)) {
+        //         ans += map.get(sum - goal);
+        //     }
+
+        //     map.put(sum, map.getOrDefault(sum, 0) + 1);
+        // }
+
+        // return ans;
     }
 }
