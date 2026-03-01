8a7a5031b3d39d376dc9dced302abb07ceb9e19e
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-03-01 14:04:24 +0530

    Time: 22 ms (42.10%) | Memory: 52.9 MB (8.64%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        // Method1: Brute O(n2)
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1); // most imp
+
+        int ans = 0;
+        int sum = 0;
+        
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            if(map.containsKey(sum - goal)) {
+                ans += map.get(sum - goal);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return ans;
+    }
+}
