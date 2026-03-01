b703bdbf41c97942c1878c9bb048b3c3ad2d2cf0
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-03-01 14:15:19 +0530

    Time: 1 ms (99.93%) | Memory: 50.8 MB (79.34%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,20 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        // Method1: Brute O(n2)
-        int n = nums.length;
-        int cnt = 0;
+        // // Method1: Brute O(n2)
+        // int n = nums.length;
+        // int cnt = 0;
         
-        for(int i = 0;  i<n; i++) {
-            int sum = 0;
-            for(int j = i; j < n; j++) {
-                sum += nums[j];
+        // for(int i = 0;  i<n; i++) {
+        //     int sum = 0;
+        //     for(int j = i; j < n; j++) {
+        //         sum += nums[j];
 
-                if(sum == goal) cnt++;
-            }
-        }
+        //         if(sum == goal) cnt++;
+        //     }
+        // }
         
-        return cnt;
+        // return cnt;
+
 
         // //method2: Better O(n) & O(n)
         // int n = nums.length;
@@ -35,5 +36,30 @@ class Solution {
         // }
 
         // return ans;
+
+
+        //Method3: Optimal O(n) & O(1)
+        return helper(nums, goal) - helper(nums, goal-1);
+        
+    }
+
+    public int helper(int[] nums, int goal) {
+        if(goal < 0) return 0;
+        int n = nums.length;
+        int l = 0, r = 0, cnt = 0, sum = 0;
+
+        while(r < n) {
+            sum += nums[r];
+
+            while(sum > goal) {
+                sum -= nums[l];
+                l++;
+            }
+
+            cnt = cnt + (r-l+1);
+            r++;
+        }
+
+        return cnt;
     }
 }
