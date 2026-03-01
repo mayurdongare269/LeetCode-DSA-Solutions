1a23c1c53127f2c5898d5818ec274f781b7219f4
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-03-01 10:25:11 +0530

    Time: 54 ms (65.55%) | Memory: 68.4 MB (89.49%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,5 +1,6 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+        // Method 2: Optimal O(n) & O(3)
         int left = 0, right = 0, maxLen = 0;
         Map<Integer, Integer> basket = new HashMap<>();
 
@@ -19,5 +20,24 @@ class Solution {
         }
 
         return maxLen;
+
+        // //Method 1 : Brute O(n2) - TLE
+        // int n = fruits.length;
+        // int maxLen = 0;
+
+        // for(int i = 0; i < n; i++) {
+        //     HashMap<Integer, Integer> map = new HashMap<>(); // basket
+        //     int currCnt = 0;
+        //     for(int j = i; j < n; j++) {
+        //         map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+
+        //         if(map.size() > 2) break;
+
+        //         currCnt++;
+        //     }
+        //     maxLen = Math.max(currCnt, maxLen);
+        // }
+
+        // return maxLen;
     }
 }
