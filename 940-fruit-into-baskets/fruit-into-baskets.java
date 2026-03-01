2434a1ccf39fe4c804b022bb215d7d4c5d694011
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-03-01 10:21:08 +0530

    Time: 56 ms (29.97%) | Memory: 68.8 MB (82.90%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -6,7 +6,7 @@ class Solution {
         while(right < fruits.length) {
             basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
 
-            while(basket.size() > 2) {
+            if(basket.size() > 2) {
                 basket.put(fruits[left], basket.get(fruits[left]) - 1);
                 if(basket.get(fruits[left]) == 0) {
                     basket.remove(fruits[left]);
