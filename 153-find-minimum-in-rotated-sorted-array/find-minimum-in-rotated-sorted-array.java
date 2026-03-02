a9f0f0bc192953a91f9f27844b94c7d998fe59b1
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-03-03 00:25:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (6.99%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,22 +1,44 @@
 class Solution {
     public int findMin(int[] nums) { // remeber it ... low < high or see YT for low <= high approach
-        int low = 0, high = nums.length - 1;
 
-        while (low < high) {
+       // // method 1: not a striver method
+        // int low = 0, high = nums.length - 1;
+
+        // while (low < high) {
+        //     int mid = low + (high - low) / 2;
+
+        //     // If mid element is greater than the rightmost, min is in right half
+        //     if (nums[mid] > nums[high]) {
+        //         low = mid + 1;
+        //     } 
+        //     // Else, the min is at mid or in the left half
+        //     else {
+        //         high = mid;
+        //     }
+        // }
+
+        // // At the end of loop, low == high and pointing to the smallest element
+        // return nums[low];
+
+
+        //Method2: striver method YT
+        int n = nums.length;
+        int low = 0, high = n - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while(low <= high) {
             int mid = low + (high - low) / 2;
 
-            // If mid element is greater than the rightmost, min is in right half
-            if (nums[mid] > nums[high]) {
+            if(nums[low] <= nums[mid]) {
+                ans = Math.min(ans, nums[low]);
                 low = mid + 1;
-            } 
-            // Else, the min is at mid or in the left half
-            else {
-                high = mid;
+            } else {
+                high = mid - 1;
+                ans = Math.min(ans, nums[mid]);
             }
         }
 
-        // At the end of loop, low == high and pointing to the smallest element
-        return nums[low];
+        return ans;
        
     }
 }
