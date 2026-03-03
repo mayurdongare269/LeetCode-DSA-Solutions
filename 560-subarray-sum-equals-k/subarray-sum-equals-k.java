75c6e301ea658a7c600bd5e3b3a79ca403cbf55a
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-03-03 20:41:08 +0530

    Time: 26 ms (33.39%) | Memory: 49.1 MB (22.25%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -21,19 +21,38 @@ class Solution {
         return ans;
 
 
-        // int left=0, sum=0;
+        // int n = nums.length;
 
-        // for(int right=0; right<n; right++){
-        //     sum += arr[right];
+        // // To store the maximum length of the subarray
+        // int maxLen = 0;
 
-        //     while(sum > target){
-        //         sum -= arr[left++];
+        // // Pointers for sliding window
+        // int left = 0, right = 0;
+
+        // // Sum of the current window
+        // int sum = nums[0];
+
+        // // Traverse through the array
+        // while (right < n) {
+
+        //     // Shrink the window if sum exceeds k
+        //     while (left <= right && sum > k) {
+        //         sum -= nums[left];
+        //         left++;
         //     }
 
-        //     if(sum == target){
-        //         System.out.println("Found");
-        //         return;
+        //     // Update max length if sum equals k
+        //     if (sum == k) {
+        //         maxLen = Math.max(maxLen, right - left + 1);
+        //     }
+
+        //     // Expand the window to the right
+        //     right++;
+        //     if (right < n) {
+        //         sum += nums[right];
         //     }
         // }
+
+        // return maxLen;
     }
 }
