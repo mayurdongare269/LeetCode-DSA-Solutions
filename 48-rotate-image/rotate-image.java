f517ac786f3bef60e63bb9cdb59cf5d85493427a
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-05-04 13:42:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (51.87%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -3,8 +3,8 @@ class Solution {
         int n = matrix.length;
 
         //1. transpose
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = i+1; j < n; j++) {
+        for(int i = 0; i < n - 1; i++) { // 0 -> n-1
+            for(int j = i+1; j < n; j++) { // i+1 -> n
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
@@ -23,8 +23,6 @@ class Solution {
                 l++;
                 r--;
             }
-            
-            
         }
     }
 }
