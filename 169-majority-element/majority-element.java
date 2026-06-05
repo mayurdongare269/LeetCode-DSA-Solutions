6ed9b9ef0b069addda622aaff038f30b28536622
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-06-05 22:27:02 +0530

    Add comment for Boyer–Moore algorithm
    Added a comment about the Boyer–Moore Majority Vote Algorithm.

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,6 +1,8 @@
 class Solution {
     // mayur dongare
     public int majorityElement(int[] nums) {
+
+        // Boyer–Moore Majority Vote Algorithm
         // int count = 0;
         // int candidate = 0;
 
