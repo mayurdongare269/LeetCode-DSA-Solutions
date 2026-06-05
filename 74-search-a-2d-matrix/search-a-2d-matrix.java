c97a0e12b5e1b16fd84985c24a6c7f75b742a16e
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-06-05 21:44:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (8.12%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;       // Number of rows
-        int n = matrix[0].length;    // Number of columns
+        int m = matrix.length; // Number of rows
+        int n = matrix[0].length;// Number of col
 
-        int left = 0, right = m * n - 1;
+        int left = 0, right = m * n - 1; // Total elements = m*n, last valid index = (m*n - 1)
 
-        while (left <= right) {
+        while(left <= right) {
             int mid = left + (right - left) / 2;
 
-            int midElement = matrix[mid / n][mid % n]; // Convert 1D index to 2D
+            // mid / n == row and mid % n == colom
+            int midElm = matrix[mid / n][mid % n]; // Convert 1D index to 2D
 
-            if (midElement == target) {
+            if(midElm == target) {
                 return true;
-            } else if (midElement < target) {
-                left = mid + 1;
+            } else if(midElm < target) {
+                left = left + 1;
             } else {
-                right = mid - 1;
+                right = right - 1;
             }
         }
 
