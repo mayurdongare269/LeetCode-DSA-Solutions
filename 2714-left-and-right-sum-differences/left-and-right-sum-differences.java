dd69f274314b1fd0d161094635d61d10c2392e29
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-06-06 20:51:52 +0530

    Time: 2 ms (98.84%) | Memory: 46.8 MB (13.15%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -1,23 +1,48 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
+        // int n = nums.length;
+
+        // int[] left = new int[n];
+        // int[] right = new int[n];
+
+        // left[0] = 0;
+        // for(int i = 1; i < n; i++) {
+        //     left[i] = left[i-1] + nums[i-1];
+        // }
+
+        // right[n-1] = 0;
+        // for(int i = n-2; i >= 0; i--) {
+        //     right[i] = right[i+1] + nums[i+1];
+        // }
+
+        // int[] ans = new int[n];
+        // for(int i = 0; i < n; i++) {
+        //     ans[i] = Math.abs(left[i] - right[i]);
+        // }
+
+        // return ans;
+
+
+        //optimized.. solved using only one array and O(1) extra space
+
         int n = nums.length;
 
-        int[] left = new int[n];
-        int[] right = new int[n];
-
-        left[0] = 0;
-        for(int i = 1; i < n; i++) {
-            left[i] = left[i-1] + nums[i-1];
-        }
-
-        right[n-1] = 0;
-        for(int i = n-2; i >= 0; i--) {
-            right[i] = right[i+1] + nums[i+1];
+        int totalSum = 0;
+        for(int num : nums) {
+            totalSum += num;
         }
 
         int[] ans = new int[n];
+
+        int leftSum = 0;
+
         for(int i = 0; i < n; i++) {
-            ans[i] = Math.abs(left[i] - right[i]);
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            ans[i] = Math.abs(leftSum - rightSum);
+
+            leftSum += nums[i];
         }
 
         return ans;
