979fcd103fd47104ea48af46cbf60a1cd69e4429
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-06-12 22:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (76.49%) - LeetSync

---
+++ 61-rotate-list/rotate-list.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k == 0) return head;
+
+        //1. locate tail at last and calculate length of list
+        ListNode tail = head;
+        int len = 1;
+
+        while(tail.next != null) { // *tail.next bz we want a last node as tail...
+            len++;
+            tail = tail.next;
+        }
+
+        if(k % len == 0) { // as there is no change
+            return head;
+        }
+
+        // 2. for large list length, we need to take it modulo
+        k = k % len;
+
+        // 3. pointers arrangement
+        tail.next = head; // attach tail to head
+        ListNode newLastNode = findNthNode(head, len - k); // remember: len - k th node we need to find
+
+        head = newLastNode.next; // if forgot - see video or dry run it...
+        newLastNode.next = null;
+
+        return head;
+    }
+
+    public static ListNode findNthNode(ListNode temp, int k) {
+        int cnt = 1;
+
+        while(temp != null) {
+            if(cnt == k) return temp;
+            cnt++;
+            temp = temp.next;
+        }
+
+        return temp;
+
+    }
+    
+}
