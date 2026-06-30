8190a593808e44ed461cc2e77101efc5efb84282
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-06-30 23:51:50 +0530

    Time: 1 ms (62.66%) | Memory: 43 MB (82.41%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,43 @@
+class Solution {
+    // 2 approached sort and one optimal...
+
+    public String longestCommonPrefix(String[] strs) {
+        // M-I: sorting TC: O(n log n + m) - easy one
+
+        Arrays.sort(strs);
+
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+
+        int i = 0;
+
+        while (i < first.length() && i < last.length()) {
+
+            if (first.charAt(i) == last.charAt(i))
+                i++;
+            else
+                break;
+        }
+
+        return first.substring(0, i);
+
+
+        // //M-II: optimal TC:(O(n x m)) -not understanded
+        // String prefix = strs[0];
+
+        // for (int i = 1; i < strs.length; i++) {
+
+        //     while (!strs[i].startsWith(prefix)) {
+
+        //         prefix = prefix.substring(0, prefix.length() - 1);
+
+        //         if (prefix.isEmpty())
+        //             return "";
+        //     }
+        // }
+
+        // return prefix;
+
+
+    }
+}
