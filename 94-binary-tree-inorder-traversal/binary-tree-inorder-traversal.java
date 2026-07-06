f8c2fed54185ca7ee32c5ec46e5429d47728376b
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-07-06 23:53:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (70.10%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -15,11 +15,64 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        inorder(root, result);
-        return result;
+        // List<Integer> result = new ArrayList<>();
+        // inorder(root, result);
+        // return result;
+
+        // // M-2: Iterative - not for pre & post
+        // Stack<TreeNode> st = new Stack<>();
+        // List<Integer> res = new ArrayList<>();
+        // TreeNode node = root;
+
+        // while(true) {
+        //     if(node != null) {
+        //         st.push(node);
+        //         node = node.left;
+        //     } else {
+        //         if(st.isEmpty()) {
+        //             break; // ends the looop
+        //         }
+
+        //         node = st.pop();
+        //         res.add(node.val);
+        //         node = node.right;
+        //     }
+        // }
+
+        // return res;
+
+
+        // M-3: Morris Inorder Traversal TC: O(2n) SC: O(1) - for preorder also,not for post
+        ArrayList<Integer> res  = new ArrayList<>();
+        TreeNode cur = root;
+
+        while(cur != null) {
+            if(cur.left == null) { // no left so add its root
+                res.add(cur.val);
+                cur = cur.right;
+            } else {
+                //if left child is not null
+                TreeNode prev = cur.left;
+                while(prev.right != null && prev.right != cur) {
+                    prev = prev.right;
+                }
+
+                if(prev.right == null) { 
+                    prev.right = cur; // attach the thread and go left.. see video if forgot
+                    cur = cur.left;
+                } else { // if already thread exists, then remove and add in res
+                    prev.right = null;
+                    res.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+        }
+
+        return res;
+
     }
 
+    //M-1 Recursion
     private void inorder(TreeNode root, List<Integer> result) {
         if(root == null) return;
 
