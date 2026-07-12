43d1a908969149e0c11a5a6925fde5f54436b5bd
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-07-12 12:13:18 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (63.01%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -10,7 +10,7 @@ class Solution {
     public void deleteNode(ListNode node) {
         // we are going to copy next node to position of given node and ref to next.next
 
-        node.val = node.next.val;
-        node.next = node.next.next;
+        node.val = node.next.val; // del node madhe next node chi value ghetali
+        node.next = node.next.next; // and as there are 2 same nodes so remove one
     }
 }
