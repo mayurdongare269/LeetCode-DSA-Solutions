f7d4167a88edce3d95a298bf6a1d6f3f4072abd7
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-07-21 11:12:53 +0530

    Time: 3 ms (93.43%) | Memory: 44.3 MB (75.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
+    public boolean isAnagram(String s1, String s2) {
         // HashMap<Character, Integer> map = new HashMap<>();
 
         // for(int i = 0; i < s.length(); i++) {
@@ -24,22 +24,42 @@ class Solution {
 
         //M2: 
 
-        s = s.toLowerCase();
-        t = t.toLowerCase();
+        // s = s.toLowerCase();
+        // t = t.toLowerCase();
 
-        if(s.length() == t.length()) {
-            char[] s1 = s.toCharArray();
-            char[] s2 = t.toCharArray();
+        // if(s.length() == t.length()) {
+        //     char[] s1 = s.toCharArray();
+        //     char[] s2 = t.toCharArray();
 
-            Arrays.sort(s1);
-            Arrays.sort(s2);
+        //     Arrays.sort(s1);
+        //     Arrays.sort(s2);
 
-            return Arrays.equals(s1, s2);
-        } else {
-            return false;
+        //     return Arrays.equals(s1, s2);
+        // } else {
+        //     return false; 
+        // }
+        //no return at last 
+        //beacuse There is no way for the program to reach the end of the method. So Java is satisfied.
+
+        //M3:
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+        if(n1 != n2) return false;
+
+        int[] word = new int[256];
+        for(int i = 0; i < n1; i++) {
+            word[s1.charAt(i)]++;
         }
 
+        for(int i = 0; i < n1; i++) {
+            word[s2.charAt(i)]--;
 
+            if(word[s2.charAt(i)] < 0) {
+                return false;
+            }
+        }
 
+        return true;
     }
 }
