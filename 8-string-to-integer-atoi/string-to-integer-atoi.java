be7c24ed6b6971910e8bdd3cae6d6b2b1ffb7fe7
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-07-21 14:05:10 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (42.04%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -2,37 +2,85 @@ class Solution {
 
     public int myAtoi(String s) {
 
-        s = s.trim();
+        //M-I: strings but its take more memmory as new string created...
+        // s = s.trim();
 
-        if (s.length() == 0)
+        // if (s.length() == 0)
+        //     return 0;
+
+        // int i = 0;
+        // int sign = 1;
+
+        // if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+
+        //     if (s.charAt(i) == '-')
+        //         sign = -1;
+
+        //     i++;
+        // }
+
+        // long num = 0;
+
+        // while (i < s.length() && Character.isDigit(s.charAt(i))) { // i < s.length() - most imp***
+
+        //     num = num * 10 + (s.charAt(i) - '0'); // convert to integer.. remember "num * 10"
+
+        //     if (sign * num <= Integer.MIN_VALUE)
+        //         return Integer.MIN_VALUE;
+
+        //     if (sign * num >= Integer.MAX_VALUE)
+        //         return Integer.MAX_VALUE;
+
+        //     i++;
+        // }
+
+        // return (int) (sign * num);
+
+        // M-II: using arr optimized
+        char[] arr = s.toCharArray();
+
+        int n = arr.length;
+        if(n == 0) {
             return 0;
+        }
 
         int i = 0;
+
+        while(i < n && arr[i] == ' ') {
+            i++;
+        }
+
+        if(i == n) { // if all whitespaces...
+            return 0;
+        }
+
         int sign = 1;
 
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-
-            if (s.charAt(i) == '-')
+        if(arr[i] == '-' || arr[i] == '+') {
+            if(arr[i] == '-') {
                 sign = -1;
-
+            }
             i++;
         }
 
-        long num = 0;
+        //loop for zeros but no need as it automaticaly handle in calculations
 
-        while (i < s.length() && Character.isDigit(s.charAt(i))) { // i < s.length() - most imp***
+        long ans = 0;
 
-            num = num * 10 + (s.charAt(i) - '0'); // convert to integer.. remember "num * 10"
-
-            if (sign * num <= Integer.MIN_VALUE)
+        while(i < n && (arr[i] >= '0' && arr[i] <= '9')) { // add condition
+            ans = ans * 10 + (arr[i] - '0'); // convert to int
+            
+            if(sign * ans < Integer.MIN_VALUE) {
                 return Integer.MIN_VALUE;
-
-            if (sign * num >= Integer.MAX_VALUE)
+            }
+            if(sign * ans > Integer.MAX_VALUE) {
                 return Integer.MAX_VALUE;
+            }
 
             i++;
         }
 
-        return (int) (sign * num);
+        return (int) (sign * ans);  
+        
     }
 }
