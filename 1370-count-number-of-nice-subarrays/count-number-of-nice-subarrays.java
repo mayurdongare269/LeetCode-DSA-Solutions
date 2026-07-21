fb36c0a8aedf871a4e24ac2a0cb37b6a5122eda7
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-07-21 22:09:09 +0530

    Time: 10 ms (88.24%) | Memory: 59.7 MB (39.78%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -2,30 +2,52 @@ class Solution {
     public int numberOfSubarrays(int[] nums, int k) { // same as lt 930
         return helper(nums, k) - helper(nums, k-1);
     }
-
-    public int helper(int[] nums, int k) {
-        if(k < 0) return 0; // bz of k - 1 ..
-
-        int l = 0, r = 0;
+    //MI:
+    public int helper(int[] nums, int goal) {
+        if(goal < 0) return 0; // bz of goal - 1...
+        
         int n = nums.length;
-        int ans = 0, cntOdd = 0;
+        int l = 0, r = 0, cnt = 0, sum = 0;
 
         while(r < n) {
-            if(nums[r] % 2 != 0) {
-                cntOdd++;
+            sum += nums[r] % 2; // changes in 930
+
+            while(sum > goal) {
+                sum -= nums[l] % 2; // changes in 930
+                l++;
             }
 
-            while(cntOdd > k) {
-                if(nums[l] % 2 != 0){
-                    cntOdd--;
-                }
-                l = l + 1;
-            }
-
-            ans = ans + (r - l + 1);
+            cnt = cnt + (r-l+1);
             r++;
-        } 
+        }
 
-        return ans;
+        return cnt;
     }
+
+    //M-II
+    // public int helper(int[] nums, int k) {
+    //     if(k < 0) return 0; // bz of k - 1 ..
+
+    //     int l = 0, r = 0;
+    //     int n = nums.length;
+    //     int ans = 0, cntOdd = 0;
+
+    //     while(r < n) {
+    //         if(nums[r] % 2 != 0) { // changes in 930
+    //             cntOdd++;
+    //         }
+
+    //         while(cntOdd > k) {
+    //             if(nums[l] % 2 != 0){ // changes in 930
+    //                 cntOdd--;
+    //             }
+    //             l = l + 1;
+    //         }
+
+    //         ans = ans + (r - l + 1);
+    //         r++;
+    //     } 
+
+    //     return ans;
+    // }
 }
