f7214a4a6da36bf598cb9257b8800c5af269a531
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-07-29 11:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.84%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,25 @@
 class Solution {
-    public int fib(int n) {
-        if (n == 0) return 0;  // base case
-        if (n == 1) return 1;  // base case
+    public int fib(int n) { 
+        //M-I: Recursive ->  TC: O(n2) SC: O(n)
+        // if (n == 0) return 0;  // base case
+        // if (n == 1) return 1;  // base case
 
-        return fib(n - 1) + fib(n - 2);
+        // return fib(n - 1) + fib(n - 2);
+
+        //MII: iterative ->  O(N) O(1)
+        if(n == 0) return 0;
+        if (n == 1) return 1;
+
+        int first = 0;
+        int second = 1;
+
+        for(int i = 2; i <= n; i++) {
+            int cur = first + second;
+            first = second;
+            second = cur;
+        }
+
+        return second;
+
     }
 }
