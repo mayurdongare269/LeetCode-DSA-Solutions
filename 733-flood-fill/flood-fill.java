14045193759c561be6b323bb1a8b2e8b4682168d
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-09-01 12:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (11.01%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,26 +1,69 @@
+import java.util.Arrays;
+
 class Solution {
 
-    public void helper(int[][] image, int sr, int sc, int color, boolean[][] vis, int orgCol) {
-        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || 
-            vis[sr][sc] || image[sr][sc] != orgCol) {
-                return;
+    public void helper(int[][] image, int sr, int sc, int color,
+                       boolean[][] vis, int orgCol) {
+
+        // Boundary + visited + different color check
+        if (sr < 0 || sc < 0 ||
+            sr >= image.length || sc >= image[0].length ||
+            vis[sr][sc] ||
+            image[sr][sc] != orgCol) {
+
+            return;
         }
+
+        // Mark visited
         vis[sr][sc] = true;
+
+        // Change color
         image[sr][sc] = color;
 
-        //left
-        helper(image, sr, sc-1, color, vis, orgCol);
-        //right
-        helper(image, sr, sc+1, color, vis, orgCol);
-        //up
-        helper(image, sr-1, sc, color, vis, orgCol);
-        //down
-        helper(image, sr+1, sc, color, vis, orgCol);
+        // Left
+        helper(image, sr, sc - 1, color, vis, orgCol);
+
+        // Right
+        helper(image, sr, sc + 1, color, vis, orgCol);
+
+        // Up
+        helper(image, sr - 1, sc, color, vis, orgCol);
+
+        // Down
+        helper(image, sr + 1, sc, color, vis, orgCol);
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] vis = new boolean[image.length][image[0].length];
-        helper(image, sr, sc, color, vis, image[sr][sc]);
+
+        boolean[][] vis =
+            new boolean[image.length][image[0].length];
+
+        int orgCol = image[sr][sc];
+
+        helper(image, sr, sc, color, vis, orgCol);
+
         return image;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] image = {
+            {1, 1, 1},
+            {1, 1, 0},
+            {1, 0, 1}
+        };
+
+        int sr = 1;
+        int sc = 1;
+        int color = 2;
+
+        Solution obj = new Solution();
+
+        int[][] result = obj.floodFill(image, sr, sc, color);
+
+        // Print result
+        for (int[] row : result) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
