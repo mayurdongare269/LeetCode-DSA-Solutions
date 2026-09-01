77a6e63433fa716d7440b6de455ebfa266bf2cfc
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-09-01 14:54:15 +0530

    Time: 2 ms (99.34%) | Memory: 47.4 MB (11.26%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,48 +1,46 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+       HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            if(map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+       for(int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if(map.containsKey(comp)) {
+                return new int[]{map.get(comp), i};
             }
-
             map.put(nums[i], i);
-        }
+       }
 
-        return new int[0];
+       return new int[]{-1, -1};
 
-        //method 2 ...
+    // M1: Brute force
+    // int n = nums.length;
 
-        // int n = nums.length;
+    // for(int i = 0; i <= n - 1; i++) {
+    //     for(int j = i + 1; j <= n - 1; j++) {
+    //         if(nums[i] + nums[j] == target) {
+    //             return new int[]{i, j};
+    //         }
+    //     }
+    // }
+    //  return new int[]{-1, -1};
 
-        // // store value + original index
-        // int[][] arr = new int[n][2];
-        // for (int i = 0; i < n; i++) {
-        //     arr[i][0] = nums[i]; // value
-        //     arr[i][1] = i;       // original index
-        // }
+        
+    // }
 
-        // // sort by value
-        // Arrays.sort(arr, (a, b) -> a[0] - b[0]);
-        // // or Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
 
-        // int l = 0, r = n - 1;
 
-        // while (l < r) {
-        //     int sum = arr[l][0] + arr[r][0];
+    // M3: but arr must sorted -> For LeetCode 1 (Two Sum), you CANNOT simply sort the array because the question asks you to return the original indices.
+    // Arrays.sort(nums);
+    // int left = 0;
+    // int right = nums.length - 1;
 
-        //     if (sum == target) {
-        //         return new int[]{arr[l][1], arr[r][1]};
-        //     } else if (sum < target) {
-        //         l++;
-        //     } else {
-        //         r--;
-        //     }
-        // }
+    // while(left < right) {
+    //     int mid = left + (right - left) / 2;
 
-        // return new int[0];
+    //     if(nums[left] + nums[right] == target) {
+    //         return new int[left, right];
+    //     } else if()
+
+    
     }
 }
