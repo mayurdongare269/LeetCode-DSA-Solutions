c6bd3709dd15d5dc97194a3785cca73114f86f16
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-09-15 18:48:01 +0530

    Time: 2234 ms (5.03%) | Memory: 183.1 MB (9.55%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -0,0 +1,57 @@
+class Solution {
+
+    public boolean isPallindrome(String s, int i, int j) {
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public int solve(String s, int k, int i, int j,  int[][] dp) {
+
+        int n = s.length();
+
+        if(i >= n || j >= n) {
+            return 0;
+        }
+        
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if(isPallindrome(s, i, j)) {
+            int takeIt = 1 + solve(s, k, j+1, j+k, dp);
+            int grow = solve(s, k, i, j+1, dp);
+            int slide = solve(s, k, i+1, j+1, dp);
+
+            return dp[i][j] = Math.max(takeIt, Math.max(grow, slide));
+        }
+
+        int grow = solve(s, k, i, j+1, dp);
+        int slide = solve(s, k, i+1, j+1, dp);
+
+        return dp[i][j] = Math.max(grow, slide);
+    }
+
+    public int maxPalindromes(String s, int k) {
+        int n = s.length();
+
+        if(k == 1) {
+            return n;
+        }
+ 
+        int[][] dp = new int[n+1][n+1];
+
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return solve(s, k, 0, k-1, dp);
+    }
+}
