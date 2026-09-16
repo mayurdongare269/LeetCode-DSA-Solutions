083aa37e285286f67877cac708426fe034ac7dbb
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-09-16 12:02:32 +0530

    Time: 29 ms (67.74%) | Memory: 56 MB (77.42%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,40 +1,97 @@
 class Solution {
 
     static final int MOD = 1000000007;
-    int[][] dp;
 
-    public int solve(int n, int k, int i) {
+    public int numberOfSets(int n, int K) {
 
-        if (k == 0)
-            return 1;
+        int[][] dp = new int[K + 1][n + 1];
 
-        if (i >= n)
-            return 0;
-
-        if (dp[k][i] != -1)
-            return dp[k][i];
-
-        long take = 0;
-
-        // Start the segment from point i
-        for (int j = i + 1; j < n; j++) {
-            take = (take + solve(n, k - 1, j)) % MOD;
+        // Base case: 0 segments = 1 way
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
         }
 
-        // Skip point i
-        long skip = solve(n, k, i + 1);
+        for (int k = 1; k <= K; k++) {
 
-        return dp[k][i] = (int)((take + skip) % MOD);
-    }
+            // Suffix sum of previous row
+            int[] suffix = new int[n + 1];
 
-    public int numberOfSets(int n, int k) {
+            for (int i = n - 1; i >= 0; i--) {
+                suffix[i] = (int) ((suffix[i + 1] + dp[k - 1][i]) % MOD);
+            }
 
-        dp = new int[k + 1][n];
+            for (int i = n - 1; i >= 0; i--) {
 
-        for (int i = 0; i <= k; i++) {
-            Arrays.fill(dp[i], -1);
+                // Take: sum of dp[k-1][j] for j = i+1 to n-1
+                int take = suffix[i + 1];
+
+                // Skip point i
+                int skip = dp[k][i + 1];
+
+                dp[k][i] = (take + skip) % MOD;
+            }
         }
 
-        return solve(n, k, 0);
+        return dp[K][0];
     }
+
+
+    /*
+    =====================================================
+    PREVIOUS 2 APPROACHES
+    =====================================================
+
+    // 1️⃣ RECURSION + MEMOIZATION
+    //
+    // int[][] memo;
+    //
+    // int solve(int n, int k, int i) {
+    //
+    //     if (k == 0)
+    //         return 1;
+    //
+    //     if (i >= n)
+    //         return 0;
+    //
+    //     if (memo[k][i] != -1)
+    //         return memo[k][i];
+    //
+    //     long take = 0;
+    //
+    //     for (int j = i + 1; j < n; j++) {
+    //         take = (take + solve(n, k - 1, j)) % MOD;
+    //     }
+    //
+    //     long skip = solve(n, k, i + 1);
+    //
+    //     return memo[k][i] =
+    //         (int)((take + skip) % MOD);
+    // }
+
+
+    // 2️⃣ BOTTOM-UP DP
+    //
+    // int[][] dp = new int[K + 1][n + 1];
+    //
+    // for (int i = 0; i < n; i++)
+    //     dp[0][i] = 1;
+    //
+    // for (int k = 1; k <= K; k++) {
+    //
+    //     for (int i = n - 1; i >= 0; i--) {
+    //
+    //         int take = 0;
+    //
+    //         for (int j = i + 1; j < n; j++) {
+    //             take = (take + dp[k - 1][j]) % MOD;
+    //         }
+    //
+    //         int skip = dp[k][i + 1];
+    //
+    //         dp[k][i] = (take + skip) % MOD;
+    //     }
+    // }
+    
+    =====================================================
+    */
 }
